a6a1d5fdd2abcab82eaad7bba5fcd0d111c27f26
unknown <webhostingportfolio124@gmail.com>
2025-10-14 16:35:45 +0530

    modified ui

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -14,11 +14,7 @@ const LatestJobCards = ({job}) => {
                 <h1 className='font-extrabold text-gray-800  text-lg my-2'>{job?.title}</h1>
                 <p className='text-sm text-gray-600'>{job?.description}</p>
             </div>
-            <div className='flex flex-wrap items-center gap-2 mt-4'>
-                {/* <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
-                <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
-                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary}LPA</Badge> */}
-
+            <div className='flex items-center gap-2 mt-4'>
                 <Badge className={'px-3 py-1 text-xs font-medium text-teal-700 bg-teal-50 rounded-md'} variant="ghost">{job?.position} Positions</Badge>
                 <Badge className={'px-3 py-1 text-xs font-medium text-white bg-green-500 rounded-md shadow-md'} variant="ghost">{job?.jobType}</Badge>
                 <Badge className={'px-3 py-1 text-xs font-bold text-cyan-700 bg-cyan-100 rounded-md'} variant="ghost">{job?.salary}LPA</Badge>
